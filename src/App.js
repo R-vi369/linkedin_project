80271c467420ba0b6da2f4d91ce37fa32e91e55f
Ravi <sama369.rk@gmail.com>
2023-04-29 14:04:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login'
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/'  element={<Login />} ></Route>
+
+          <Route path='/Header' element={<Header/> }/>
+          <Route path='/home' element={<Home/> }  />
+            
+           
+          
+
+
+        </Routes>
+      </Router >
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+const Home = (props) => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
