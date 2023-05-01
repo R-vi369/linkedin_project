efe5bd8717b8587da5568dc36eb35cc9ff0d2410
Ravi <sama369.rk@gmail.com>
2023-05-01 17:36:06 +0530

    wroking on main components

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Main = () => {
   return (
     <Container>
-      <ShareBox>Share</ShareBox>
+      <ShareBox>Share
       <div>
         <img src="/images/user.svg" alt="" />
         <button>Start a post</button>
@@ -30,6 +30,24 @@ const Main = () => {
           <img src="/images/article-icon.svg" alt="" />
           <span>Write Article</span>
         </button>
+      </div>
+      </ShareBox>
+      <div>
+        <Article>
+        <ShareActor>
+          <a>
+            <img src="/images/user.svg" alt="" />
+            <div>
+              <span>Title</span>
+              <span>Info</span>
+              <span>Date</span>
+            </div>
+          </a>
+          <button>
+            <img src="/images/ellips.png" alt="" />
+          </button>
+        </ShareActor>
+        </Article>
       </div>
     </Container>
   )
@@ -61,9 +79,49 @@ div{
     color:rgba(0,0,0 0.6);
     font-size: 14px;
     line-height: 1.5;
-    min-height: 40px;
+    min-height: 48px;
     background:transparent;
+    border: none;
+    display:flex;
+    align-items: center;
+    font-weight:600;
+  }
+  &:first-child{
+    display:flex;
+    align-items: center;
+    padding: 8px 16px 8px 16px;
+    img{
+      width:48px;
+      border-radius: 50%;
+      margin-right:8px
+    }
+    button{
+      margin:4px 0;
+      flex-grow:1;
+      border-radius: 50%;
+      padding-left: 16px;
+      border: 1px solid rgba(0,0,0,0.15);
+      border-radius: 36px;
+      background-color:white;
+      text-align:left;
 
+    }
+  }
+
+  &:nth-child(2){
+    display: flex;
+    flex-wrap:wrap;
+    justify-content: space-around;
+    padding-bottom: 4px;
+
+    button{
+      img{
+        margin:0 4px 0 -2px;
+      }
+      span{
+        color:#70b5f9;
+      }
+    }
   }
 }
 
@@ -71,6 +129,63 @@ div{
 `;
 
 
+const Article= styled(CommonCard)`
+padding:0;
+margin: 0 0 8px;
+overflow:visible;
+`;
+
+
+const ShareActor = styled.div`
+
+padding-right: 40px;
+flex-wrap: nowrap;
+padding:12px 16px;
+margin-bottom:8px;
+align-items:center;
+display:flex;
+a{
+  margin-right:12px;
+  flex-grow:1;
+  overflow:hidden;
+  display:flex;
+  text-decoration: none;
+  img{
+    width:40px;
+    height:48px;
+  }
+  &> div{
+    display:flex;
+    flex-direction:column;
+    flex-grow:1;
+    flex-basis:0;
+    margin-left:8px;
+    overflow:hidden;
+    span{
+      text-align:left;
+      &:fisrt-child{
+        font-size:14px;
+        font-weight: 700;
+        color:rgba(0,0,0, 1);
+      }
+      &:nth-child(n+1){
+        font-size: 12px;
+        color:rgba(0,0,0, 0.6)
+      }
+    }
+  }
+}
+
+button{
+  position:absolute;
+  right:12px;
+  top:0;
+  background: transparent;
+  border:none;
+  outline:none;
+  
+}
+`;
 
 
 export default Main
